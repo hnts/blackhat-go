83be8660febe1bc340e7b1b10dfd5fdaec23c729
hntsr <hanatsu0824@gmail.com>
2020-02-08 21:52:29 +0900

    using worker pool

--- port-scanner/main.go
+++ port-scanner/main.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"sync"
 )
 
+func worker(ports chan int, wg *sync.WaitGroup) {
+	for p := range ports {
+		fmt.Println(p)
+		wg.Done()
+	}
+}
+
 func main() {
+	ports := make(chan int, 100)
 	var wg sync.WaitGroup
+
+	for i:= 0; i < cap(ports); i++ {
+		go worker(ports, &wg)
+	}
+
 	for i := 1; i <= 1024; i++ {
 		wg.Add(1)
-		go func(j int) {
-			defer wg.Done()
-			address := fmt.Sprintf("scanme.nmap.org:%d", j)
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				return
-			}
-			conn.Close()
-			fmt.Printf("%d open\n", j)
-		}(i)
+		ports <- i
 	}
 	wg.Wait()
+	close(ports)
 }
