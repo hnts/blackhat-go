17f5c5b0e9f1677b1b6bd9e1065ca492238b977e
hntsr <hanatsu0824@gmail.com>
2020-02-09 13:04:06 +0900

    multi channell communication

--- port-scanner/main.go
+++ port-scanner/main.go
@@ -2,28 +2,50 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"net"
+	"sort"
 )
 
-func worker(ports chan int, wg *sync.WaitGroup) {
+func worker(ports, results chan int) {
 	for p := range ports {
-		fmt.Println(p)
-		wg.Done()
+		address := fmt.Sprintf("scanme.nmap.org:%d", p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- p
 	}
 }
 
 func main() {
 	ports := make(chan int, 100)
-	var wg sync.WaitGroup
+	results := make(chan int)
+	var openports []int
 
-	for i:= 0; i < cap(ports); i++ {
-		go worker(ports, &wg)
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
 	}
 
-	for i := 1; i <= 1024; i++ {
-		wg.Add(1)
-		ports <- i
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 1024; i++ {
+		port := <- results
+		if port != 0 {
+			openports = append(openports, port)
+		}
 	}
-	wg.Wait()
+
 	close(ports)
+	close(results)
+
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d open\n", port)
+	}
 }
