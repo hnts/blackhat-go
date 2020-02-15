213ace4cc7100d1b8150a41e973e31ba902085c5
hntsr <hanatsu0824@gmail.com>
2020-02-15 13:51:10 +0900

    create simple proxy server

---
+++ proxy-server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net"
+	"io"
+)
+
+func handle(src net.Conn) {
+	dst, err := net.Dial("tcp", "joescatcam.website:80")
+	if err != nil {
+		log.Fatalln("Unable to connect to our unreachable host")
+	}
+	defer dst.Close()
+
+	go func() {
+		if _, err := io.Copy(dst, src); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	if _, err := io.Copy(src, dst); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":20080")
+	if err != nil {
+		log.Fatalln("Unable to bind to port")
+	}
+	log.Println("Listening on 127.0.0.1:80")
+	for {
+		conn, err := listener.Accept()
+		log.Println("Received connection")
+		if err != nil {
+			log.Fatalln("Unable to accept connection")
+		}
+		go handle(conn)
+	}
+
+}
